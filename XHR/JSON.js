79dbbd53f5b0426af3e4b27a515b2308039c36d8
Matt Toft <matttoft@gmail.com>
2011-12-01 23:18:34 -0500

    project 1ish
    not working yet :(

---
+++ XHR/JSON.js
@@ -0,0 +1,71 @@
+//Matt Toft
+//ASD - 1112
+//JSON Populate Examples
+
+{"techteam":[
+        "Techmem1": {
+            "First: ", "Matt",
+            "Last: ", "Toft",
+           "Email: ", "mtoft@cinci.rr.com",
+            "Phone #:","513-555-0559",
+            "Sex: ", "Male",
+            "Ministry:","Tech",
+           "Attending (Months):", "7",
+           "Partner: ", "Yes",
+            "Member Type: ", "Transplant",
+            "Status:", "Member",
+        },
+        "Techmem2": {
+        	"fName": ["First: ", "Craig"],
+            "lName": ["Last: ", "Krenzel"],
+            "email": ["Email: ", "ckrenzel@cinci.rr.com"],
+            "tel": ["Phone #:","513-555-0553"],
+            "sex": ["Sex: ", "Male"],
+            "dropdown":["Ministry:","Tech"],
+            "attending": ["Attending (Months):", "16"],
+            "partner": ["Partner: ", "Yes"],
+            "memtype": ["Member Type: ", "Transplant"],
+            "status": ["Status:", "Member"],
+        },
+         "Techmem3": {
+           	"fName": ["First: ", "Michael"],
+            "lName": ["Last: ", "Jenkins"],
+            "email": ["Email: ", "mjenkins@cinci.rr.com"],
+            "tel": ["Phone #:","513-555-0550"],
+            "sex": ["Sex: ", "Male"],
+            "dropdown":["Ministry:","Tech"],
+            "attending": ["Attending (Months):", "12"],
+            "partner": ["Partner: ", "Yes"],
+            "memtype": ["Member Type: ", "Transplant"],
+            "status": ["Status:", "Member"],
+        },
+         
+            "Techlead1": {
+           	"fName": ["First: ", "Jon"],
+            "lName": ["Last: ", "Sams"],
+            "email": ["Email: ", "jsams@cinci.rr.com"],
+            "tel": ["Phone #:","513-555-0555"],
+            "sex": ["Sex: ", "Male"],
+            "dropdown":["Ministry:","Tech"],
+            "attending": ["Attending (Months):", "3"],
+            "partner": ["Partner: ", "Yes"],
+            "memtype": ["Member Type: ", "Transplant"],
+            "status": ["Status:", "Leader"],
+        },
+        
+             "Techlead2": {
+           	"fName": ["First: ", "Paul"],
+            "lName": ["Last: ", "Brown"],
+            "email": ["Email: ", "pbrown@cinci.rr.com"],
+            "tel": ["Phone #:","513-555-0554"],
+            "sex": ["Sex: ", "Male"],
+            "dropdown":["Ministry:","Tech"],
+            "attending": ["Attending (Months):", "27"],
+            "partner": ["Partner: ", "Yes"],
+            "memtype": ["Member Type: ", "Transplant"],
+            "status": ["Status:", "Leader"],
+        },
+        ]
+        
+    };
+
--- js/site.js
+++ js/site.js
@@ -3,14 +3,36 @@
  * ASD 1112
  */
 
-$(document).ready(function(){
-//site code	
+$(document).ready(function() {
+	//site code
 
-
-	
-	
-	
-	
-	
-	
+	$.ajax({
+		type : "GET",
+		url : "xhr/data.xml",
+		dataType : "xml",
+		complete : function() {
+			$('#impressions').listview('refresh');
+		},
+		success : function(xml) {
+			$(xml).find('item').each(function() {
+				var day = $(this).find('day').text();
+				var task = $(this).find('task').text();
+				$('<li data-role="list-divider" ></li>').html(day).appendTo('#impressions');
+				$('<li data-theme="d" ></li>').html(task).appendTo('#specList');
+			});
+		}
+	});
+	//Load CSV Data I think this is not kosher for the csv but not 100% sure
+	$('#CSV').empty();
+	$('#CSV').CSVToTable('xhr/data.csv');
+});
+//Getting data from a JSON file
+$.getJSON('xhr/data.json', function(data) {
+	var techteam = [];
+	$.each(data, function(key, val) {
+		techteam.push('<li id="' + key + '">' + val + '</li>');
+	});
+	$('<li/>', {
+		html : techteam.join('')
+	}).appendTo('#tech');
 });
