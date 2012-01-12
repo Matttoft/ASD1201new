61ad0336bc029ec6e0dbe7983e61f237ed545522
Matt Toft <matttoft@gmail.com>
2012-01-12 15:21:18 -0500

    project 2

--- js/site.js
+++ js/site.js
@@ -5,6 +5,7 @@
 
 $(document).ready(function() {
 	//site code
+<<<<<<< HEAD
 
 	$.ajax({
 		type : "GET",
@@ -23,14 +24,56 @@ $(document).ready(function() {
 		}
 	});
 
+=======
+    $('#click1 a').click(function(){
+        $('#mytest').empty();
+        $.ajax({
+            url: 'xhr/data.json',
+            type: 'GET',
+            dataType: 'json',
+            success: function(response){
+                for (var i=0, j=response.customers.length; i<j; i++){
+                    var cust2= response.customers[i];
+                    $(''+
+                        '<li>'+
+                            '<h3 class="ui-li-aside">'+ cust2.phone +'</h3>'+
+                            '<h2>'+ cust2.name +'</h2>'+
+                            '<p>'+ cust2.address +'</p>'+
+                            
+                        '</li>'
+                    ).appendTo('#mytest');
+                };
+            }
+        });
+    });
+    $('#click2 a').click(function(){
+        $('#mytest').empty();
+        $.ajax({
+            url: 'xhr/orders.xml',
+            type: 'GET',
+            dataType: 'xml',
+            success: function(response2){
+                $(response2).find('order').each(function(){
+                    var name_txt = $(this).find('name').text()
+                    var addr_txt = $(this).find('addr').text()
+                    var phone_txt = $(this).find('phone').text()
+                    var item_txt = $(this).find('item').text()
+                    $(''+
+                        '<li>'+
+                            '<h3 class="ui-li-aside">'+ phone_txt +'</h3>'+
+                            '<h2>'+ name_txt +'</h2>'+
+                            '<p>'+ addr_txt +'</p>'+
+                            '<p>'+ item_txt +'</p>'+
+                        '</li>'
+                    ).appendTo('#mytest');
+                });
+            }
+        });
+    });
+    $('#click3 a').click(function(){
+        $('#mytest').empty();
+        $('#mytest').CSVToTable('xhr/test.csv');
+    });
+>>>>>>> master
 });
-//Getting data from a JSON file
-$.getJSON('xhr/data.json', function(data) {
-	var techteam = [];
-	$.each(data, function(key, val) {
-		techteam.push('<li id="' + key + '">' + val + '</li>');
-	});
-	$('<li/>', {
-		html : techteam.join('')
-	}).appendTo('#tech');
 });
