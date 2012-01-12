f8f53fb0156a7fd648882f47c2c57f5f6e7af4df
Matt Toft <matttoft@gmail.com>
2012-01-12 15:20:28 -0500

    project2

--- js/site.js
+++ js/site.js
@@ -5,32 +5,54 @@
 
 $(document).ready(function() {
 	//site code
-
-	$.ajax({
-		type : "GET",
-		url : "xhr/data.xml",
-		dataType : "xml",
-		complete : function() {
-			$('#impressions').listview('refresh');
-		},
-		success : function(xml) {
-			$(xml).find('item').each(function() {
-				var day = $(this).find('day').text();
-				var task = $(this).find('task').text();
-				$('<li data-role="list-divider" ></li>').html(day).appendTo('#impressions');
-				$('<li data-theme="d" ></li>').html(task).appendTo('#specList');
-			});
-		}
-	});
-
+    $('#click1 a').click(function(){
+        $('#mytest').empty();
+        $.ajax({
+            url: 'xhr/data.json',
+            type: 'GET',
+            dataType: 'json',
+            success: function(response){
+                for (var i=0, j=response.customers.length; i<j; i++){
+                    var cust2= response.customers[i];
+                    $(''+
+                        '<li>'+
+                            '<h3 class="ui-li-aside">'+ cust2.phone +'</h3>'+
+                            '<h2>'+ cust2.name +'</h2>'+
+                            '<p>'+ cust2.address +'</p>'+
+                            
+                        '</li>'
+                    ).appendTo('#mytest');
+                };
+            }
+        });
+    });
+    $('#click2 a').click(function(){
+        $('#mytest').empty();
+        $.ajax({
+            url: 'xhr/orders.xml',
+            type: 'GET',
+            dataType: 'xml',
+            success: function(response2){
+                $(response2).find('order').each(function(){
+                    var name_txt = $(this).find('name').text()
+                    var addr_txt = $(this).find('addr').text()
+                    var phone_txt = $(this).find('phone').text()
+                    var item_txt = $(this).find('item').text()
+                    $(''+
+                        '<li>'+
+                            '<h3 class="ui-li-aside">'+ phone_txt +'</h3>'+
+                            '<h2>'+ name_txt +'</h2>'+
+                            '<p>'+ addr_txt +'</p>'+
+                            '<p>'+ item_txt +'</p>'+
+                        '</li>'
+                    ).appendTo('#mytest');
+                });
+            }
+        });
+    });
+    $('#click3 a').click(function(){
+        $('#mytest').empty();
+        $('#mytest').CSVToTable('xhr/test.csv');
+    });
 });
-//Getting data from a JSON file
-$.getJSON('xhr/data.json', function(data) {
-	var techteam = [];
-	$.each(data, function(key, val) {
-		techteam.push('<li id="' + key + '">' + val + '</li>');
-	});
-	$('<li/>', {
-		html : techteam.join('')
-	}).appendTo('#tech');
 });
