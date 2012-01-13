8b30afd413ac5801b1b7c32bf79e39ec49470b76
Matt Toft <matttoft@gmail.com>
2012-01-12 23:04:56 -0500

    week 2

--- js/main.js
+++ js/main.js
@@ -5,184 +5,35 @@
 
  */
 $(document).ready(function(){
-<<<<<<< HEAD
 	
 	var formLock = $(".lockup").length;
 
 
-=======
-<<<<<<< HEAD
-
-/*	function $(x) {
-		var theElement = document.getElementById(x);
-		return theElement;
-} */
-
-<<<<<<< HEAD
-	function makeList() {
-=======
-/*	function makeList() {
->>>>>>> gh-pages
-		var formTag = $("form");
-		var selectLi = $('#select');
-		var makeSelect $(document.createElement('select'));
-		makeSelect.setAttribute("id", "dropdown");
-		for(var i = 0, j = ministry.length; i < j; i++) {
-			var makeOption = document.createElement('option');
-			var optText = ministry[i];
-			makeOption.setAttribute("value", optionText);
-			makeOption.html (optionText);
-			makeSelect.appendTo(makeOption);
-		}
-		selectLi.appendTo(makeSelect);
-<<<<<<< HEAD
-	}
-=======
-}; */
->>>>>>> gh-pages
-
-/*	function getradio() {
-		var radio = document.forms[0].sex;
-		for(var i = 0; i < radio.length; i++) {
-			if(radio[i].checked) {
-				sexval = radio[i].val();
-			}
-		}
-
-} */
-
-	function getcheckval() {
-		if($('checkbox-1').checked) {
-			memtypeval = $('checkbox-1').val();
-		} else {
-			memtypeval = "No"
-		}
-	}
-
-	function toggleControls(n) {
-		switch(n) {
-			case "on":
-				$('minform').css = "none";
-				$('clear').css = "inline";
-				$('displayLink').css = "none";
-				$('addnew').css = "inline";
-				break;
-			case "off":
-				$('minform').css = "block";
-				$('clear').css = "inline";
-				$('displayLink').css = "inline";
-				$('addnew').css = "none";
-				$('items').css = "none";
-				break;
-			default:
-				return false;
-		}
-	}
-
-	function storeData(key) {
-		if(!key) {
-=======
-	
-	var formLock = $(".lockup").length;
-
-
->>>>>>> master
 	$('#submit').live('click',function(key) {
 		var xkey=this.key;
 		var id='';
 		
 		if(!xkey) {
-<<<<<<< HEAD
-=======
->>>>>>> gh-pages
->>>>>>> master
 			var id = Math.floor(Math.random() * 100000001);
 		} else {
 			id = xkey;
 		}
-<<<<<<< HEAD
 
 		var item = {};
-=======
-<<<<<<< HEAD
-	//	getradio();
-	//	getcheckval();
-		var item = {};
-<<<<<<< HEAD
-		item.fname = ["First Name:", $('#fname').value];
-		item.lname = ["Last Name:", $('#lname').value];
-		item.email = ["Email:", $('#email').value];
-		item.tel = ["Telephone #:", $('#tel').value];
-		item.radios = ["Sex:", sexval];
-		item.group = ["Ministry:", $('#dropdown').value];
-		item.attending = ["Attending Months:", $('#attending').value];
-		item.partner = ["Partner:", memtypeval];
-		item.memtype = ["Member Type:", $('#checkbox-1').value];
-		item.status = ["Status:", $('#status').value];
-		localStorage.setItem(id, JSON.stringify(item));
-=======
-=======
-
-		var item = {};
->>>>>>> gh-pages
->>>>>>> master
 		item.fname = ["First Name:", $('#fname').val()];
 		item.lname = ["Last Name:", $('#lname').val()];
 		item.email = ["Email:", $('#email').val()];
 		item.tel = ["Telephone #:", $('#tel').val()];
-<<<<<<< HEAD
 		item.sex =["Sex:",$('input:radio[name=sex]:checked').val()];
 		item.group = ["Ministry:", $('#dropdown').val()];
 		item.attending = ["Attending Months:", $('#attending').val()];
 		item.memtype = ["Member Type:", $('#checkbox').val()];
 		
-		sessionStorage.setItem(id, JSON.stringify(item));
+		localStorage.setItem(id, JSON.stringify(item));
 		alert("Data submitted, a ministry leader will contact you soon.");
-	//	$.mobile.changePage( 'additem.html', {reloadPage: true},{ allowSamePageTranstion: true},{ transition: 'none'});
+
 	});
 	
-=======
-<<<<<<< HEAD
-	//	item.radios = ["Sex:", sexval];
-		item.group = ["Ministry:", $('#dropdown').val()];
-		item.attending = ["Attending Months:", $('#attending').val()];
-	//	item.partner = ["Partner:", memtypeval];
-		item.memtype = ["Member Type:", $('#checkbox-1').val()];
-		
-		sessionStorage.setItem(id, JSON.stringify(item));
->>>>>>> gh-pages
-		alert("Data submitted, a ministry leader will contact you soon.");
-		$.mobile.changePage( 'additem.html', {reloadPage: true},{ allowSamePageTranstion: true},{ transition: 'none'});
-	}
-
-	function getdata() {
-	       $('#pullDiv').append(
-			
-			'<li>Name: ' + item.fname.toUpperCase()+ " " item.lname.toUpperCase() + '</li>' + 
-			'<li>Email: ' + item.email + '</li>' +
-			'<li>Telephone: ' + item.tel + '</li>' +
-			'<li>Ministry: ' + item.dropdown + '</li>' +
-			'<li>Months Attending: ' + item.attending + '</li>' + 
-			'<li>Member Type: ' + item.checkbox-1 + '</li> ' +
-			'<li>Partner: ' + item.memtype +'</li>'
-		);
-	sessionStorage.getItem("id", JSON.stringify());
-}); 
-$('#pullDiv').listview('refresh');
-
-=======
-		item.sex =["Sex:",$('input:radio[name=sex]:checked').val()];
-		item.group = ["Ministry:", $('#dropdown').val()];
-		item.attending = ["Attending Months:", $('#attending').val()];
-		item.memtype = ["Member Type:", $('#checkbox').val()];
-		
-		sessionStorage.setItem(id, JSON.stringify(item));
-		alert("Data submitted, a ministry leader will contact you soon.");
-	//	$.mobile.changePage( 'additem.html', {reloadPage: true},{ allowSamePageTranstion: true},{ transition: 'none'});
-	});
-	
->>>>>>> gh-pages
->>>>>>> master
 
 	function getimage(catname, makesublist) {
 		var imageli = document.createElement('li');
@@ -200,166 +51,6 @@ $('#pullDiv').listview('refresh');
     };  
 
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-/*	function autofilldata() {
-		var json = {
-			'contact1' : {
-				"fname" : ["First Name:", "Matt"],
-				"lname" : ["Last Name:", "Toft"],
-				"email" : ["Email:", "matttoft@fullsail.edu"],
-				"tel" : ["Telephone:", "513-555-0559"],
-				"radios" : ["Sex:", "male"],
-				"dropdown" : ["Ministry:", "Nursery"],
-				'attending' : ['Attending Months:', "6"],
-				'partner' : ["Partner:", "Yes"],
-				'checkbox-1' : ["Member Type:", "Transplant"],
-				
-			},
-			'contact2' : {
-				"fname" : ["First Name:", "Kim"],
-				"lname" : ["Last Name:", "Toft"],
-				"email" : ["Email:", "osulilbit@aol.com"],
-				"tel" : ["Telephone:", "513-555-0559"],
-				"radios" : ["Sex:", "female"],
-				"dropdown" : ["Ministry:", "Outreach"],
-				'attending' : ['Attending Months:', "6"],
-				'partner' : ["Partner:", "Yes"],
-				'checkbox-1' : ["Member Type:", "Transplant"],
-				
-			}
-		};
-		for(var n in json) {
-			var id = Math.floor(Math.random() * 100000001);
-			localStorage.setItem(id, JSON.stringify(json[n]));
-		}
-} */
-
-	function makeitemlinks(key, linksli) {
-		var editlink = document.createElement('a');
-		editlink.href = "#";
-		editlink.key = key;
-		var edittext = "Edit Contact";
-		editlink.addEventListener("click", edititem);
-		editlink.innerHTML = edittext;
-		linksli.appendChild(editlink);
-
-		var breaktag = document.createElement('br');
-		linksli.appendChild(breaktag);
-
-		var dellink = document.createElement('a');
-		dellink.href = "#";
-		dellink.key = key;
-		var deltext = "Delete Contact";
-		dellink.addEventListener("click", delitem);
-		dellink.innerHTML = deltext;
-		linksli.appendChild(dellink);
-
-	}
-
-	function edititem() {
-		var value = localStorage.getItem(this.key);
-		var item = JSON.parse(value);
-
-		toggleControls("off");
-
-		$('#fname').val(item.fname[1]);
-		$('#lname').val(item.lname[1]);
-		$('#email').val(item.email[1]);
-		$('#tel').val(item.tel[1]);
-		
-		var radios = document.forms[0].sex;
-		for(var i = 0; i < radios.length; i++) {
-			if(radios[i].val() == 'male' && item.radios[1] == "male") {
-				radios[i].setAttribute("checked", "checked");
-			} else if(radios[i].val() == 'female' && item.radios[1] == "female") {
-				radios[i].setAttribute("checked", "checked");
-			}
-		}
-		$('#dropdown').val(item.dropdown[1]);
-<<<<<<< HEAD
-		$('#attending').value(item.attending[1]);
-=======
-		$('#attending').val()(item.attending[1]);
->>>>>>> gh-pages
-		if(item.partner[1] == 'yes') {
-			$('#partner').setAttribute("checked", "checked");
-		}
-		$('#checkbox-1').val(item.memtype[1]);
-		$('#story').val(item.story[1]);
-
-		saveEntry.unbind("click", storeData);
-
-<<<<<<< HEAD
-		$('#submit').value = "Edit Contact";
-=======
-		$('#submit').val() = "Edit Contact";
->>>>>>> gh-pages
-		var editsubmit = $('#submit');
-		editsubmit.addEventListener("click", validate);
-		editsubmit.key = this.key;
-
-	}
-
-	function delitem() {
-		var ask = confirm("Are you sure?");
-		if(ask) {
-			localStorage.removeItem(this.key);
-			alert("Item Deleted.")
-			window.location.reload();
-		} else {
-			alert("Item Not Deleted.")
-		}
-	}
-
-	   function clearLocal() {
-        if (sessionStorage.length === 0) {
-            alert("There is nothing in Storage");
-        } else {
-            sessionStorage.clear();
-            alert("Data has been cleared");
-            return false;
-        }
-    }
-
-
-	function validate(e) {
-		var getgroup = $('#dropdown');
-		var getfname = $('#fname');
-		var getlname = $('#lname');
-		var getemail = $('#email');
-
-	errmsg.css("display", "none");
-		$("#dropdown").css ("border","");
-		$("#fname").css ("border","");
-		$("#lname").css ("border","");
-		$("#email").css ("border","");
-
-		var messagearray = [];
-		if(getgroup.val === "") {
-			var grouperror = 'Please Choose a Ministry Option.';
-			getgroup.style.border = "3px solid red";
-			messagearray.push(grouperror);
-		}
-		if(getfname.val() === "") {
-			var fnameerror = 'Please Enter a First Name.';
-			getfname.style.border = "3px solid red";
-			messagearray.push(fnameerror);
-		}
-		if(getlname.val() === "") {
-			var lnameerror = 'Please Enter a Last Name.';
-			getlname.style.border = "3px solid red";
-			messagearray.push(lnameerror);
-		}
-		var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-		if(!(re.exec(getemail.val()))) {
-			var emailerror = "Please Enter a Valid Email Address";
-			getemail.style.border = "3px solid red";
-			messagearray.push(emailerror);
-=======
->>>>>>> gh-pages
->>>>>>> master
 
 	$("#fname").live("blur", function(){
 		if (this.value === "") {
@@ -367,7 +58,7 @@ $('#pullDiv').listview('refresh');
             $("#fnameErr").show();
 		};
 		if(this.value !== "") {
-			$(this).css("border", "solid 1px green");
+			$(this).css("border", "solid 1px black");
 			$("#fnameErr").remove();
 		};
 		lok();         
@@ -379,19 +70,11 @@ $('#pullDiv').listview('refresh');
             $("#lnameErr").show();
 		};
 		if(this.value !== "") {
-			$(this).css("border", "solid 1px green");
+			$(this).css("border", "solid 1px black");
 			$("#lnameErr").remove();		
 		};
 		lok();
 	});
-
-	$("#male").live("change", function(){
-		$("#genderErr").remove();
-	});
-	$("#female").live("change", function(){
-		$("#genderErr").remove();
-	});
-	
 
 	$("#dropdown").live("change", function(){
 		if (this.value === "") {
--- js/site.js
+++ js/site.js
@@ -5,75 +5,107 @@
 
 $(document).ready(function() {
 	//site code
-<<<<<<< HEAD
-
+	
+		
+		
+$('#json').bind('click', function(){
+	$('#browse').empty();
 	$.ajax({
-		type : "GET",
-		url : "xhr/data.xml",
-		dataType : "xml",
-		complete : function() {
-			$('#impressions').listview('refresh');
-		},
-		success : function(xml) {
-			$(xml).find('item').each(function() {
-				var day = $(this).find('day').text();
-				var task = $(this).find('task').text();
-				$('<li data-role="list-divider" ></li>').html(day).appendTo('#impressions');
-				$('<li data-theme="d" ></li>').html(task).appendTo('#specList');
+		url: 'XHR/data.json',
+		type: 'GET',
+		dataType: 'json',
+		success: function(response){
+        	for (var i=0, j=response.techteam.length; i<j; i++){
+				var jdata = response.techteam[i];
+				$(''+
+					'<li class="member">'+
+						'<h2>'+ jdata.fname +'</h2>'+
+						'<h3>'+ jdata.ministry +'</h3>'+
+						'<h4>'+ jdata.email +'</h4>'+
+						'<h4>'+ jdata.tel +'</h4>'+
+					'</li><hr />'
+				).appendTo('#browse');
+				console.log(response);
+			}
+		}
+	});
+	return false;
+});
+
+
+   $('#xml').bind('click', function(){
+	$('#browse').empty();
+	$.ajax({
+		url: 'xhr/data.xml',
+		type: 'GET',
+		dataType: 'xml',
+		success: function(xml){
+			$(xml).find("mem").each(function(){
+   				var name = $(this).find('fname').text();
+   				var email= $(this).find('email').text();
+   				var tel= $(this).find('phone').text();
+				var min= $(this).find('min').text();
+    			$(''+
+					'<li class="member">'+
+						'<h2>'+ name+'</h2>'+
+						'<h3>'+ min +'</h3>'+
+						'<h4>'+ email +'</h4>'+
+						'<h4>'+ tel+'</h4>'+				
+
+						
+					'</li><hr />'
+				).appendTo('#browse');
+				console.log(xml);
 			});
 		}
 	});
+	return false;
+});
 
-=======
-    $('#click1 a').click(function(){
-        $('#mytest').empty();
-        $.ajax({
-            url: 'xhr/data.json',
-            type: 'GET',
-            dataType: 'json',
-            success: function(response){
-                for (var i=0, j=response.customers.length; i<j; i++){
-                    var cust2= response.customers[i];
-                    $(''+
-                        '<li>'+
-                            '<h3 class="ui-li-aside">'+ cust2.phone +'</h3>'+
-                            '<h2>'+ cust2.name +'</h2>'+
-                            '<p>'+ cust2.address +'</p>'+
-                            
-                        '</li>'
-                    ).appendTo('#mytest');
-                };
-            }
-        });
-    });
-    $('#click2 a').click(function(){
-        $('#mytest').empty();
-        $.ajax({
-            url: 'xhr/orders.xml',
-            type: 'GET',
-            dataType: 'xml',
-            success: function(response2){
-                $(response2).find('order').each(function(){
-                    var name_txt = $(this).find('name').text()
-                    var addr_txt = $(this).find('addr').text()
-                    var phone_txt = $(this).find('phone').text()
-                    var item_txt = $(this).find('item').text()
-                    $(''+
-                        '<li>'+
-                            '<h3 class="ui-li-aside">'+ phone_txt +'</h3>'+
-                            '<h2>'+ name_txt +'</h2>'+
-                            '<p>'+ addr_txt +'</p>'+
-                            '<p>'+ item_txt +'</p>'+
-                        '</li>'
-                    ).appendTo('#mytest');
-                });
-            }
-        });
-    });
-    $('#click3 a').click(function(){
-        $('#mytest').empty();
-        $('#mytest').CSVToTable('xhr/test.csv');
-    });
->>>>>>> master
+	$('#csv').bind('click', function(){
+	$('#browse').empty();
+	 $.ajax({
+        type: "GET",
+        url: "XHR/data.csv",
+        dataType: "text",
+        success: function(data) {
+        	var allTextLines = data.split(/\r\n|\n/);
+    		var headers = allTextLines[0].split(',');
+    		var lines = []; 
+
+			for (var i=1; i<allTextLines.length; i++) {
+				var data = allTextLines[i].split(',');
+				if (data.length == headers.length) {
+					var members= []; 
+					
+					for (var j=0; j<headers.length; j++) {
+						members.push(data[j]);
+					}
+					lines.push(members); 
+				}
+				
+			}
+			
+			for (var m=0; m<lines.length; m++){
+				var member = lines[m];
+			$(''+
+					'<li class="memebrs">'+
+						'<h2>'+ member[0] +'</h2>'+
+						'<h4>'+ member[5] +'</h4>'+
+						'<h4>'+ member[2] +'</h4>'+
+						'<h4>'+ member[3] +'</h4>'+
+					'</li><hr />'
+				).appendTo('#browse');
+			console.log(lines);	
+			}
+        }
+	});
+	return false;
 });
-});
+
+		
+		
+		
+		
+	});
+		
